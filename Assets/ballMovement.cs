9649fa826f9f4aba6c6b97685d82af72ea574ca7
SrVaderXD <henriquelfaria08@gmail.com>
2021-07-27 13:31:13 -0300

    Bricks feature: physics and logic

--- Assets/ballMovement.cs
+++ Assets/ballMovement.cs
@@ -18,18 +18,41 @@ public class ballMovement : MonoBehaviour
     void Update()
     {
         transform.position = new Vector2(transform.position.x + (dx * speed), transform.position.y + (dy * speed));
+        checkDefeat();
+        GameObject[] bricks = GameObject.FindGameObjectsWithTag("brick");
+
+        if (bricks.Length == 0)
+        {
+            Debug.Log("Win!!");
+        }
+
+        Debug.Log((dx * speed));
+        Debug.Log((dy * speed));
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
 
+        float angle = 0;
+
         if (collision.gameObject.tag == "Player")
         {
-            float angle = Random.Range(-45f, 45f);
+
+            if (Random.Range(0, 100f) < 50f)
+            {
+                angle = Random.Range(20f, 45f);
+            }
+            else
+            {
+                angle = Random.Range(90f + 20f, 90f + 45f);
+
+            }
+
             dx = Mathf.Cos(Mathf.Deg2Rad * angle);
             dy = Mathf.Sin(Mathf.Deg2Rad * angle);
 
-            if(dy < 0){
+            if (dy < 0)
+            {
                 dy *= -1;
             }
 
@@ -38,6 +61,25 @@ public class ballMovement : MonoBehaviour
         else if (collision.gameObject.tag == "wall_left" || collision.gameObject.tag == "wall_right")
         {
             dx = -1;
+        }
+        else if (collision.gameObject.tag == "brick")
+        {
+            dy = -1;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "unbreakable_brick")
+        {
+            dy = -1;
+        }
+
+
+    }
+
+    void checkDefeat()
+    {
+        if (transform.position.y < GameObject.FindGameObjectWithTag("Player").transform.position.y)
+        {
+            Application.LoadLevel(Application.loadedLevel);
         }
     }
 }
