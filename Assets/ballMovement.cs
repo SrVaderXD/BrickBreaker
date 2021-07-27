2f6bc22c9ef2531074247fa47feb7fa1e782147f
SrVaderXD <henriquelfaria08@gmail.com>
2021-07-27 18:30:02 -0300

    Ball physics fix

--- Assets/ballMovement.cs
+++ Assets/ballMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ballMovement : MonoBehaviour
 {
-    public float dx = 0, dy = -0.01f;
-    public float speed = 2f;
-
+    public float dx = 0;
+    public float dy = -0.01f;
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class ballMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x + (dx * speed), transform.position.y + (dy * speed));
+        transform.position = new Vector2(transform.position.x + (dx*speed),transform.position.y + (dy*speed));
         checkDefeat();
         GameObject[] bricks = GameObject.FindGameObjectsWithTag("brick");
 
@@ -25,27 +25,24 @@ public class ballMovement : MonoBehaviour
         {
             Debug.Log("Win!!");
         }
-
-        Debug.Log((dx * speed));
-        Debug.Log((dy * speed));
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
 
-        float angle = 0;
-
         if (collision.gameObject.tag == "Player")
         {
 
-            if (Random.Range(0, 100f) < 50f)
+            float angle = 0;
+
+            if (Random.Range(0,100f) < 50f)
             {
                 angle = Random.Range(20f, 45f);
             }
+
             else
             {
                 angle = Random.Range(90f + 20f, 90f + 45f);
-
             }
 
             dx = Mathf.Cos(Mathf.Deg2Rad * angle);
@@ -55,29 +52,26 @@ public class ballMovement : MonoBehaviour
             {
                 dy *= -1;
             }
-
-            speed = 0.2f;
+            speed = 0.1f;
         }
         else if (collision.gameObject.tag == "wall_left" || collision.gameObject.tag == "wall_right")
         {
-            dx = -1;
+            dx *= -1;
         }
         else if (collision.gameObject.tag == "brick")
         {
-            dy = -1;
+            dy *= -1;
             Destroy(collision.gameObject);
         }
         else if (collision.gameObject.tag == "unbreakable_brick")
         {
-            dy = -1;
+            dy *= -1;
         }
-
-
     }
 
     void checkDefeat()
     {
-        if (transform.position.y < GameObject.FindGameObjectWithTag("Player").transform.position.y)
+        if (transform.position.y < GameObject.FindGameObjectWithTag("Player").transform.position.y  - 5)
         {
             Application.LoadLevel(Application.loadedLevel);
         }
